6bdb8585a70cace9f471d4c72e8b35f167a78642
Lorenzo McDaniel <lsm3x@mtmail.mtsu.edu>
2020-05-04 10:33:36 -0500

    May 4th updates
    worked on teacherlookup page

--- blog/urls.py
+++ blog/urls.py
@@ -11,6 +11,7 @@ from django.urls import path
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('teacher_lookup/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
