a45cdb7f02d7ccc9908bf9fcf92e7be78f41242d
Lorenzo McDaniel <lsm3x@mtmail.mtsu.edu>
2020-05-08 10:00:10 -0500

    Filter Fixed
    fixed the filter for our search pages. It searches users by username only at the moment. The filter also includes a button to reset filter to view all users again.

--- blog/filters.py
+++ blog/filters.py
@@ -3,9 +3,14 @@ import django_filters
 from .models import *
 from django.contrib.auth.models import User
 
+# this is class for the filters on pages suchs as teacherlookup
+
 
 class OrderFilter(django_filters.FilterSet):
     class Meta:
 
         model = User
-        fields = ['username', 'email']
+
+        # here you can add other fields to filter by that
+        # are based on the User model
+        fields = ['username']
