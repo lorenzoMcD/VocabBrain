58a59c3ef5490856dfe433580579acf1ccc01920
DHatton1422 <DHatton1422@users.noreply.github.com>
2020-06-04 17:59:50 -0400

    Jumbled game update
    Scrambles words if specified. Got to figure out how to make it scramble the value in the div

---
+++ blog/static/js/jumbled.js
@@ -0,0 +1,28 @@
+
+
+function jumbled(words){
+	// declare variable
+	var shuffled = "", len, i, randPos;
+	// Check if words are strings
+	words = "" + words;
+	// length of string
+	len = words.length;
+	// Change items in words to have a random position
+	for (i=0; i<len; i++){
+
+		randPos = Math.floor(Math.random() * words.length); 
+		shuffled += words[randPos];
+		words = words.slice(0, randPos) + words.slice(randPos +1); 
+
+	}
+	//output 
+	return shuffled; 
+} 	
+	let words = document.querySelector("text");
+	var shuffled = jumbled(words); 
+
+
+
+
+	console.log(shuffled)
+
