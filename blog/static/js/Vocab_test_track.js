87b84db29960f83bf1dee25c8e0b5cbb64fc3a11
DHatton1422 <DHatton1422@users.noreply.github.com>
2020-06-18 13:47:59 -0400

    fix for test

--- blog/static/js/Vocab_test_track.js
+++ blog/static/js/Vocab_test_track.js
@@ -1,4 +1,7 @@
 // vocab matching game 5 words for terms and sentences/definitions
+
+let wrong = "";
+
 let data1 = document.getElementById("data1"); 
 let data2 = document.getElementById("data2"); 
 let data3 = document.getElementById("data3"); 
@@ -7,7 +10,6 @@ let data5 = document.getElementById("data5");
 
 
 
-var customalert = document.getElementById("customAlert");
 
 
 
@@ -37,45 +39,40 @@ $(document).ready(function() {
 
     if (answertext.toLowerCase() === useranswer1.toLowerCase()) {
       correct++;
-      data1.style.color = "#0ead38";
       showOnScreen();
     }else{
-      data1.style.color = "#EB1111"
+      word = useranswer1 + ',';
+      document.getElementById("w").value += word;
     }
-
-
     if (answertext2.toLowerCase() === useranswer2.toLowerCase()){
       correct++;
-      data2.style.color = "#0ead38";
       showOnScreen();
     }else{
-      data2.style.color = "#EB1111"
+      word = useranswer2 + ',';
+    document.getElementById("w").value += word;
+    
     }
-
     if (answertext3.toLowerCase() === useranswer3.toLowerCase()){
-      correct++;
-     data3.style.color = "#0ead38";
-      showOnScreen();
+    correct++;
+    showOnScreen();
     }else{
-      data3.style.color = "#EB1111"
+      word = useranswer3 + ',';
+    document.getElementById("w").value += word;
     }
-
     if (answertext4.toLowerCase() === useranswer4.toLowerCase()){
       correct++;
-      data4.style.color = "#0ead38";
       showOnScreen();
     }else{
-      data4.style.color = "#EB1111"
+      word = useranswer4 + ',';
+    document.getElementById("w").value += word;
     }
-
     if (answertext5.toLowerCase() === useranswer5.toLowerCase()){
       correct++;
-      data5.style.color = "#0ead38";      
       showOnScreen();
     }else{
-      data5.style.color = "#EB1111"
-    }
-
+      word = useranswer5 + ',';
+    document.getElementById("w").value += word;
+  }
 
   });
 
@@ -85,41 +82,22 @@ $(document).ready(function() {
 // Defining correct answers and our button
 let correct = 0
 let clickme = document.getElementById("clickme");
-let back = document.getElementById("back"); 
 
 
 //constant function that show and updates score
 // Capping the score at 5
 const showOnScreen = () => {
-  document.getElementById("content").innerHTML = "You've scored "  + correct+ " out of 5!" ;
+  document.getElementById('correct').innerHTML = correct; 
+  document.getElementById("score").value = correct; 
   correct = Math.min(Math.max(correct,0),4);
-
-
 }
 
 // Click submit - Submit button disabled, reset button enabled
 const finish = () =>{
   let clickme = document.getElementById("clickme");
   let home = document.getElementById("home");
-  let reset = document.getElementById("reset");
   clickme.disabled = true;
-  reset.hidden = false;
-  home.hidden= false;
-  showCustom();
-
  
-}
-
-
-var customalert = document.getElementById("customAlert");
-
-// Custom Alert function 
-function showCustom(){
-  customalert.style.display = 'block';
-}
-
-function hideCustom(){
-  customalert.style.display = 'none';
 }
 
 
